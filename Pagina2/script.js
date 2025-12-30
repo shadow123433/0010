3e73e630dddce216695de63c109286c20d36a9cd
Thiago <thiagoribeirodossantos965@gmail.com>
2025-12-30 07:13:39 -0300

    ajustando

--- Pagina2/script.js
+++ Pagina2/script.js
@@ -31,22 +31,22 @@ function showToast(msg) {
 ========================= */
 const produtosPorEscola = {
   IFES: [
-    { nome: "Camisa masculina IFES", preco: 49.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemIFES/camisa.avif" },
-    { nome: "Short masculino IFES", preco: 79.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemIFES/bermuda.avif" },
-    { nome: "Camisa feminina IFES", preco: 129.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemIFES/camisaF.webp" },
-    { nome: "Calça feminina IFES", preco: 89.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemIFES/calça.png" }
+    { nome: "Camisa masculina IFES", preco: 49.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemIFES/camisa.avif" },
+    { nome: "Short masculino IFES", preco: 79.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemIFES/bermuda.avif" },
+    { nome: "Camisa feminina IFES", preco: 129.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemIFES/camisaF.webp" },
+    { nome: "Calça feminina IFES", preco: 89.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemIFES/calça.png" }
   ],
   SESI: [
-    { nome: "Camisa masculina SESI", preco: 59.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemSESI/camisa.jpg" },
-    { nome: "Bermuda SESI", preco: 84.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemSESI/bermuda.jpg" },
-    { nome: "Camisa feminina SESI", preco: 59.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemSESI/camisaF.jpg" },
-    { nome: "Calça feminina SESI", preco: 84.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemSESI/calça.webp" }
+    { nome: "Camisa masculina SESI", preco: 59.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemSESI/camisa.jpg" },
+    { nome: "Bermuda SESI", preco: 84.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemSESI/bermuda.jpg" },
+    { nome: "Camisa feminina SESI", preco: 59.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemSESI/camisaF.jpg" },
+    { nome: "Calça feminina SESI", preco: 84.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemSESI/calça.webp" }
   ],
   "CRISTO REI": [
-    { nome: "Camisa masculina CRISTO REI", preco: 54.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemCRISTOREI/camisa.jpg" },
-    { nome: "Bermuda masculina CRISTO REI", preco: 92.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemCRISTOREI/camisa.jpg" },
-    { nome: "Camisa feminina CRISTO REI", preco: 54.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemCRISTOREI/camisa.jpg" },
-    { nome: "Calça feminina CRISTO REI", preco: 92.9, tamanhos: ["P", "M", "G", "GG"], imagem: "/imagemCRISTOREI/calça.webp" }
+    { nome: "Camisa masculina CRISTO REI", preco: 54.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemCRISTOREI/camisa.jpg" },
+    { nome: "Bermuda masculina CRISTO REI", preco: 92.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemCRISTOREI/camisa.jpg" },
+    { nome: "Camisa feminina CRISTO REI", preco: 54.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemCRISTOREI/camisa.jpg" },
+    { nome: "Calça feminina CRISTO REI", preco: 92.9, tamanhos: ["P", "M", "G", "GG"], imagem: "../imagemCRISTOREI/calça.webp" }
   ]
 };
 
