6cef03574453a227b9b40651162dde770fce9131
Thiago martins <thiagoribeirodossantos965@gmail.com>
2026-01-30 23:19:24 -0300

    alterando

--- login-usuarios/auth.js
+++ login-usuarios/auth.js
@@ -178,3 +178,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+
+
+function toggleSenha() {
+  const senhaInput = document.getElementById("senha");
+  senhaInput.type = senhaInput.type === "password" ? "text" : "password";
+}
